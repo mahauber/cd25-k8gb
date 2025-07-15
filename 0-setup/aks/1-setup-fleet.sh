be3976d0ca39d3a48803ec7ae98c8e10c03a8d2d
Matthias Hauber <matthias.hauber@prodyna.com>
2025-07-15 19:56:33 +0200

    Refactor AKS setup scripts to use FluxCD for Helm chart management and add new manifests for k8gb and ingress-nginx

--- 0-setup/aks/1-setup-fleet.sh
+++ 0-setup/aks/1-setup-fleet.sh
@@ -5,45 +5,9 @@ az fleet get-credentials --resource-group rg-aks-fleetmanager --name fleetmanage
 
 kubectl create namespace managed
 
-helm repo add k8gb https://www.k8gb.io
+helm repo add fluxcd-community https://fluxcd-community.github.io/helm-charts
 helm repo update
-helm install my-k8gb k8gb/k8gb --version 0.14.0 --namespace managed
+helm upgrade --install my-flux2 fluxcd-community/flux2 --version 2.16.2 --namespace managed
 
-# docs: https://learn.microsoft.com/en-us/azure/kubernetes-fleet/concepts-resource-propagation
-kubectl apply -f - <<EOF
-apiVersion: placement.kubernetes-fleet.io/v1
-kind: ClusterResourcePlacement
-metadata:
-  name: managed
-spec:
-  resourceSelectors:
-    - group: ""
-      kind: Namespace
-      version: v1
-      name: managed
-    # cluster scoped resources need to be selected explicitly, wrapping in envelope objects is also possible https://learn.microsoft.com/en-us/azure/kubernetes-fleet/quickstart-envelope-reserved-resources
-    - group: apiextensions.k8s.io
-      version: v1
-      kind: CustomResourceDefinition
-      name: gslbs.k8gb.absa.oss
-  policy:
-    placementType: PickAll
-EOF
-
-
-# examples
-# apiVersion: placement.kubernetes-fleet.io/v1
-# kind: ClusterResourcePlacement
-# metadata:
-#   name: crp-fixed
-# spec:
-#   policy:
-#     placementType: PickFixed
-#     clusterNames:
-#     - cluster1
-#     - cluster2
-#   resourceSelectors:
-#     - group: ""
-#       kind: Namespace
-#       name: test-deployment
-#       version: v1
+kubectl apply -f ./manifests/k8gb-helmchart.yaml
+kubectl apply -f ./manifests/fluxcd-placement.yaml
